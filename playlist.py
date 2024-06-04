292a36590652d056a0ccbba2f81f3287958e624e
VinitaPatil2005 <147422005+VinitaPatil2005@users.noreply.github.com>
2024-06-04 11:07:42 +0530

    Add files via upload

---
+++ playlist.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import os
+
+st.header("Your Emotion Based Playlist")
+
+# Extract mood from URL query parameters
+query_params = st.experimental_get_query_params()
+mood = query_params.get("mood", [None])[0]
+
+if mood:
+    st.success(f"Here are some {mood} songs for you")
+    song_list = os.listdir(f"./song/{mood}/")
+    str_song_path = f"./song/{mood}/"
+    for i in song_list:
+        st.write(i.replace(".mp3", "") + ":")
+        st.audio(str_song_path + i)
+else:
+    st.warning("Mood not specified in the URL. Please go back and select a mood.")
